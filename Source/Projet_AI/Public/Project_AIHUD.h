4938491fd6c053d51bee0d13c41ce737653a20e2
Coralie <coralie.tremblay12@icloud.com>
2024-11-15 10:36:39 -0500

    Création Classes Base

---
+++ Source/Projet_AI/Public/Project_AIHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "Project_AIHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJET_AI_API AProject_AIHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
