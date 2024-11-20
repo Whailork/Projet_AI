5227da2b291a95e314222e39727b2ca32c3c5a67
Pepen <ti-max__1998@hotmail.com>
2024-11-20 13:31:07 -0500

    Feat
    Feat : Initial plugin Commit

---
+++ Plugins/CookingFactory/Source/CookingFactory/Public/Data/IngredientTable.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include"GameplayTagContainer.h"
+#include "IngredientTable.generated.h"
+
+/**
+ * 
+ */
+USTRUCT()
+struct FIngredientTable :  public FTableRowBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Plugins/CookingFactory/Source/CookingFactory/Public/Data/RecipeTable.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include"GameplayTagContainer.h"
+#include "RecipeTable.generated.h"
+
+
+USTRUCT()
+struct FRecipeTable : public FTableRowBase
+{
+	GENERATED_BODY()
+};
